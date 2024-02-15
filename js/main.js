69f40efb04076106320d886b87884a8df65abeab
yamap <yamap.web@gmail.com>
2024-02-16 01:28:53 +0900

    ⚡️

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-'use strict';
-// カウントアップ機能の実装
+const counter = document.querySelector("#counter");
+const up = document.querySelector("#up");
+const down = document.querySelector("#down");
+let count = 0;
 
-let counter = document.querySelector('#counter');
-let up = document.querySelector('#up');
-let down = document.querySelector('#down')
-let count = 0
-
-up.addEventListener('click', function(){
+up.addEventListener("click", function () {
   count += 1;
   if (count > 100) {
-      count = 0;
-    }
+    count = 0;
+  }
   counter.innerHTML = count;
-})
-down.addEventListener('click', function(){
+});
+
+down.addEventListener("click", function () {
   count -= 1;
   if (count < 0) {
-      count = 0;
-    }
+    count = 0;
+  }
   counter.innerHTML = count;
-})
+});
