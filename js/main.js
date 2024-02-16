04eb6c80e73caf44bd4bf9231c6d174febb04e1e
yamap <yamap.web@gmail.com>
2024-02-16 09:26:46 +0900

    JSに注釈を記述

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-const counter = document.querySelector("#counter");
-const up = document.querySelector("#up");
-const down = document.querySelector("#down");
+// DOMの取得
+const counter = document.getElementById("counter");
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+
+// 変数を宣言
 let count = 0;
 
+// 「カウントアップ」をクリックした時に数字を「1」増やす処理
 up.addEventListener("click", function () {
-  count += 1;
-  if (count > 100) {
-    count = 0;
-  }
-  counter.innerHTML = count;
+  count += 1; // カウントを1増やす
+  if (count > 100) count = 0; // カウントが100を超えたら0に戻す
+  counter.textContent = count; // カウントを画面に表示
 });
 
+// 「カウントダウン」をクリックした時に数字を「1」減らす処理
 down.addEventListener("click", function () {
-  count -= 1;
-  if (count < 0) {
-    count = 0;
-  }
-  counter.innerHTML = count;
+  count -= 1; // カウントを1減らす
+  if (count < 0) count = 0; // カウントが0未満になったら0に戻す
+  counter.textContent = count; // カウントを画面に表示
 });
