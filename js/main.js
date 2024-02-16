30d956429059df567f6e520d4db055fdb5812287
yamap <yamap.web@gmail.com>
2024-02-16 12:13:22 +0900

    JSの教材としての修正

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+"use strict"; // Strictモードを使用
 
 // DOMの取得
 const counter = document.getElementById("counter");
@@ -11,13 +11,17 @@ let count = 0;
 // 「カウントアップ」をクリックした時に数字を「1」増やす処理
 up.addEventListener("click", () => {
   count += 1; // カウントを1増やす
-  if (count > 100) count = 0; // カウントが100を超えたら0に戻す
-  counter.textContent = count; // カウントを画面に表示
+  if (count > 100) {
+    count = 0;
+  } // カウントが100を超えたら0に戻す
+  counter.innerHTML = count; // カウントを画面に表示
 });
 
 // 「カウントダウン」をクリックした時に数字を「1」減らす処理
 down.addEventListener("click", () => {
   count -= 1; // カウントを1減らす
-  if (count < 0) count = 0; // カウントが0未満になったら0に戻す
-  counter.textContent = count; // カウントを画面に表示
+  if (count < 0) {
+    count = 0;
+  } // カウントが0未満になったら0に戻す
+  counter.innerHTML = count; // カウントを画面に表示
 });
